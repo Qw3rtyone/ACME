46d2b7674f1aaa3a6a605503495678193f57c687
Qw3rtyone <mathew.aswin@ymail.com>
2018-11-22 00:29:56 +0000

    Initial Menu
    Simple main menu

---
+++ GoldDigger/Assets/Scripts/MenuNav.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuNav : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /**
+     * Go to the GameScene when the play button is clicked
+     */
+    public void OnClickPlay()
+    {
+        SceneManager.LoadScene(1);
+    }
+    /**
+     * Quit the application when the quit button is clicked
+     */
+    public void OnClickQuit()
+    {
+        Application.Quit();
+    }
+}
